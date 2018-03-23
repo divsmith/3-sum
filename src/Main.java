e2e1502bd9722ae812bf82be3e548f6f00d892ba
Parker Smith <parker@parkersmith.us>
2018-03-22 20:02:36 -0600

    Commit 3-sum

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static int count(int[] a)
+    {
+        int N = a.length;
+        int count = 0;
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = i + 1; j < N; j++)
+            {
+                for (int k = j + 1; k < N; k++)
+                {
+                    if (a[i] + a[j] + a[k] == 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        In in = new In(new File(args[0]));
+        int[] a = in.readAllInts();
+        System.out.println("Total ints: " + a.length);
+        Stopwatch stopwatch = new Stopwatch();
+        System.out.println(count(a));
+        double time = stopwatch.elapsedTime();
+        System.out.println("Time: " + time);
+    }
+}
